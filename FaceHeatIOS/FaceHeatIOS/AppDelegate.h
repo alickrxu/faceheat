aa536b1df44329aa1b909d07255f642ce5db27e0
Emil Daijiro Grønnbeck <emilgroenn@gmail.com>
2015-02-19 17:30:55 -0800

    created project

---
+++ FaceHeatIOS/FaceHeatIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceHeatIOS
+//
+//  Created by Daijiro on 2/19/15.
+//  Copyright (c) 2015 Team3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
