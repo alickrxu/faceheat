bd65a270dd58110fdc5430495eb9af579515e08f
alickrxu <alickrxu@gmail.com>
2015-03-01 17:20:24 -0800

    added some UI elements

--- FaceHeatIOS/FaceHeatIOS/ViewController.h
+++ FaceHeatIOS/FaceHeatIOS/ViewController.h
@@ -2,13 +2,15 @@
 //  ViewController.h
 //  FaceHeatIOS
 //
-//  Created by Daijiro on 2/19/15.
+//  Created by Alick, Daijiro on 2/19/15.
 //  Copyright (c) 2015 Team3. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#import <FLIROneSDK/FLIROneSDK.h>
+
+@interface ViewController : UIViewController <FLIROneSDKImageReceiverDelegate, FLIROneSDKStreamManagerDelegate, FLIROneSDKVideoRendererDelegate, FLIROneSDKImageEditorDelegate>
 
 
 @end
--- FaceHeatIOS/FaceHeatIOS/ViewController.m
+++ FaceHeatIOS/FaceHeatIOS/ViewController.m
@@ -2,13 +2,35 @@
 //  ViewController.m
 //  FaceHeatIOS
 //
-//  Created by Daijiro on 2/19/15.
+//  Created by Alick, Daijiro on 2/19/15.
 //  Copyright (c) 2015 Team3. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+//The main viewfinder for the FLIR ONE
+@property (weak, nonatomic) IBOutlet UIView *masterImageView;
+
+//labels for various camera information
+@property (strong, nonatomic) UIView *hottestPoint;
+@property (strong, nonatomic) UILabel *hottestLabel;
+
+@property (strong, nonatomic) NSData *thermalData;
+@property (nonatomic) CGSize thermalSize;
+
+//buttons for interacting with the FLIR ONE
+//capture video
+@property (nonatomic, strong) IBOutlet UIButton *captureVideoButton;
+
+
+
+//data for UI to display
+@property (strong, nonatomic) UIImage *thermalImage;
+
+@property (nonatomic) FLIROneSDKTuningState tuningState;
+
 
 @end
 
